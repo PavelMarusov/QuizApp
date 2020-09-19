0d0d6055d2fe4ca11564189a119aa21067b263a4
PavelMarusov <pavelmarusovs@gmail.com>
2020-09-19 12:45:31 +0600

    доп.задание дз1

--- app/src/main/java/com/example/quizapp/ui/fragment/main/MainFragment.java
+++ app/src/main/java/com/example/quizapp/ui/fragment/main/MainFragment.java
@@ -1,6 +1,7 @@
 package com.example.quizapp.ui.fragment.main;
 
 import androidx.appcompat.widget.AppCompatSeekBar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
@@ -15,12 +16,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.quizapp.R;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    private TextView counter, mainCounter;
+    private AppCompatSeekBar seekBar;
+    private Button plus_btn, minus_btn;
+
+
     public static MainFragment newInstance() {
         return new MainFragment();
     }
@@ -28,8 +37,12 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.main_fragment, container, false);
-
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        counter = view.findViewById(R.id.seek_bar_counter);
+        mainCounter = view.findViewById(R.id.main_counter);
+        seekBar = view.findViewById(R.id.main_fr_seekBar);
+        plus_btn = view.findViewById(R.id.main_plus_btn);
+        minus_btn = view.findViewById(R.id.main_minus_btn);
         return view;
     }
 
@@ -37,7 +50,18 @@ public class MainFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        Log.d("pop","Main FR");
+        mViewModel.onProgressChange(seekBar,counter);
+        mViewModel.plusPress(plus_btn);
+        mViewModel.minusPressed(minus_btn);
+        mViewModel.mutableLiveData.observe(getActivity(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                mainCounter.setText(String.valueOf(integer));
+            }
+        });
+
+
     }
+
 
 }
--- app/src/main/java/com/example/quizapp/ui/fragment/main/MainViewModel.java
+++ app/src/main/java/com/example/quizapp/ui/fragment/main/MainViewModel.java
@@ -1,7 +1,56 @@
 package com.example.quizapp.ui.fragment.main;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatSeekBar;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.lang.reflect.Array;
+
 public class MainViewModel extends ViewModel {
+    public MutableLiveData<Integer> mutableLiveData = new MutableLiveData<>();
+        final int[] num = {0};
+
+    public void plusPress(Button plus) {
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mutableLiveData.postValue(num[0]++);
+                Log.d("pop", "plus="+ num[0]);
+            }
+        });
+    }
+    public void minusPressed(Button minus){
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mutableLiveData.postValue(num[0] --);
+            }
+        });
+    }
+
+    public void onProgressChange(AppCompatSeekBar seekBar, final TextView counter) {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                counter.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
 
 }
